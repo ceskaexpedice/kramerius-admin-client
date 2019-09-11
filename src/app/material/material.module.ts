37ed25dba6bc636a079a0c99f57d085782ef7782
Jan Rychtar <honza.rychtar@gmail.com>
2019-09-11 09:30:52 +0200

    Add navbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 const components = [
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule
 ];
 
 @NgModule({
