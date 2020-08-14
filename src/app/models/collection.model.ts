68968c4b0e9870e902735efa70f38c655a33811d
Jan Rychtar <honza.rychtar@gmail.com>
2020-08-14 17:16:25 +0200

    Oprava nenacteni content ze sbirky

--- src/app/models/collection.model.ts
+++ src/app/models/collection.model.ts
@@ -41,6 +41,7 @@ export class Collection {
     collection.id = json['pid'];
     collection.name = json['name'];
     collection.description = json['description'];
+    collection.content = json['content'];
     collection.standalone = json['standalone'];
     if (json['created']) {
       collection.createdAt = new Date(json['created']);
