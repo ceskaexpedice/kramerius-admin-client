aa7f42cfb15c1ddbd791945f12385f6f10197312
Martin Řehánek <martin.rehanek@gmail.com>
2021-02-01 15:21:52 +0100

    add some content to home page

--- src/app/services/app-settings.ts
+++ src/app/services/app-settings.ts
@@ -4,21 +4,24 @@ import { Injectable } from '@angular/core';
 export class AppSettings {
 
   devMode = true; //pokud true, tak se zobrazuje zalozka Test a dalsi testovaci veci (napr. spousteni testovaciho procesu)
+  version = "1.0" + (this.devMode ? "-dev" : "");
+
 
   //kramerius.dev.digitallibrary.cz
-  //clientApiBaseUrl = 'https://kramerius.dev.digitallibrary.cz/search/api/client/v6.0';
-  //adminApiBaseUrl = 'https://kramerius.dev.digitallibrary.cz/search/api/admin/v1.0';
-  //digitalLibraryBaseUrl = 'https://dev.digitallibrary.cz/d/'; 
+  //digitalLibraryBaseUrl = 'https://dev.digitallibrary.cz/d/';
+  //coreBaseUrl = 'https://kramerius.dev.digitallibrary.cz/search/'
 
   //k7-test.mzk.cz
-  //clientApiBaseUrl = 'http://k7-test.mzk.cz/search/api/client/v6.0';
-  //adminApiBaseUrl = 'http://k7-test.mzk.cz/search/api/admin/v1.0';
-  //digitalLibraryBaseUrl = 'https://k7-test.mzk.cz/'; 
+  //digitalLibraryBaseUrl = 'https://k7-test.mzk.cz/';
+  //coreBaseUrl = 'http://k7-test.mzk.cz/search/'
 
   //localhost
-  clientApiBaseUrl = 'http://localhost:8080/search/api/client/v6.0';
-  adminApiBaseUrl = 'http://localhost:8080/search/api/admin/v1.0';
   digitalLibraryBaseUrl = 'http://www.digitalniknihovna.cz/mzk'; //TODO
+  coreBaseUrl = 'http://localhost:8080/search/'
+
+
+  clientApiBaseUrl = this.coreBaseUrl + 'api/client/v6.0';
+  adminApiBaseUrl = this.coreBaseUrl + 'api/admin/v1.0';
 
   constructor() {
   }
