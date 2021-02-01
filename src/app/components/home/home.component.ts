18437a21586813d5812f019d58956373b35ec722
Martin Řehánek <martin.rehanek@gmail.com>
2021-02-01 15:21:52 +0100

    add some content to home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppSettings } from 'src/app/services/app-settings';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appSettings: AppSettings) { }
 
   ngOnInit() {
+  }
+
+  getVersion() {
+    return this.appSettings.version;
   }
 
 }
--- src/app/services/app-settings.ts
+++ src/app/services/app-settings.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 export class AppSettings {
 
   devMode = true; //pokud true, tak se zobrazuje zalozka Test a dalsi testovaci veci (napr. spousteni testovaciho procesu)
+  version = "1.0" + (this.devMode ? "-dev" : "");
 
   //kramerius.dev.digitallibrary.cz
   //clientApiBaseUrl = 'https://kramerius.dev.digitallibrary.cz/search/api/client/v6.0';
@@ -13,7 +14,7 @@ export class AppSettings {
   //k7-test.mzk.cz
   //clientApiBaseUrl = 'http://k7-test.mzk.cz/search/api/client/v6.0';
   //adminApiBaseUrl = 'http://k7-test.mzk.cz/search/api/admin/v1.0';
-  //digitalLibraryBaseUrl = 'https://k7-test.mzk.cz/'; 
+  //digitalLibraryBaseUrl = 'https://k7-test.mzk.cz/';
 
   //localhost
   clientApiBaseUrl = 'http://localhost:8080/search/api/client/v6.0';
