63e78b3c1ad03466c871fa950d3946afaad9392a
Martin Řehánek <martin.rehanek@gmail.com>
2021-03-06 19:35:27 +0100

    increment version

--- src/app/services/app-settings.ts
+++ src/app/services/app-settings.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AppSettings {
 
-  devMode = true; //pokud true, tak se zobrazuje zalozka Test a dalsi testovaci veci (napr. spousteni testovaciho procesu)
-  version = "1.6" + (this.devMode ? "-dev" : "");
+  devMode = false; //pokud true, tak se zobrazuje zalozka Test a dalsi testovaci veci (napr. spousteni testovaciho procesu)
+  version = "1.7" + (this.devMode ? "-dev" : "");
 
   //kramerius.dev.digitallibrary.cz
   //digitalLibraryBaseUrl = 'https://dev.digitallibrary.cz/d/';
