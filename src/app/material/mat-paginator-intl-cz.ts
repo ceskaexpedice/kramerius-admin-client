17147c65c1bdf1d2e767193bbac0325b5f33b4d6
Jan Rychtar <honza.rychtar@gmail.com>
2021-11-29 15:51:38 +0100

    update to Angular 9

--- src/app/material/mat-paginator-intl-cz.ts
+++ src/app/material/mat-paginator-intl-cz.ts
@@ -1,5 +1,7 @@
 import { MatPaginatorIntl } from '@angular/material';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class MatPaginatorIntlCz extends MatPaginatorIntl {
     itemsPerPageLabel = 'Položek na stránku';
     nextPageLabel = 'Další stránka';
