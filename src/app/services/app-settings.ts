2bd5a765f0b015110d7f6b94ad93cd3a426d1a8a
Jan Rychtar <honza.rychtar@gmail.com>
2022-04-20 16:48:24 +0200

    Verze 1.1.4

--- src/app/services/app-settings.ts
+++ src/app/services/app-settings.ts
@@ -6,13 +6,13 @@ declare var APP_GLOBAL: any;
 export class AppSettings {
 
   // external properties from /assets/shared/globals.js
-  public userClientBaseUrl = APP_GLOBAL.userClientBaseUrl;
-  public coreBaseUrl = APP_GLOBAL.coreBaseUrl;
-  public keycloak = APP_GLOBAL.keycloak;
+  userClientBaseUrl = APP_GLOBAL.userClientBaseUrl;
+  coreBaseUrl = APP_GLOBAL.coreBaseUrl;
+  keycloak = APP_GLOBAL.keycloak;
 
 
-  devMode = false; //pokud true, tak se zobrazuje zalozka DEV a dalsi testovaci veci (napr. spousteni testovaciho procesu)
-  version = "1.1.3" + (this.devMode ? "-dev" : "");
+  devMode = !!APP_GLOBAL.devMode; //pokud true, tak se zobrazuje zalozka DEV a dalsi testovaci veci (napr. spousteni testovaciho procesu)
+  version = "1.1.4" + (this.devMode ? "-dev" : "");
 
   clientApiBaseUrl = this.coreBaseUrl + '/api/client/v7.0';
   adminApiBaseUrl = this.coreBaseUrl + '/api/admin/v7.0';
