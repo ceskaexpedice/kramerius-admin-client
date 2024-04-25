be29856b854e07e6767bb037d34c8cc2d0ba1063
Petr Kudela <kudy.samba@gmail.com>
2024-04-25 13:21:45 +0200

    https://github.com/ceskaexpedice/ceska-digitalni-knihovna/issues/61 -  znovu sklidit konkretni pidy - dialog - pridat checkbox "Smazat objekt", navigace / cdk - pridat novou zalozku "reharvest objektu", navigace / cdk / reharvest objektu - implementace tabulky

--- src/app/pages/cdk/cdk-object-reharvest/cdk-object-reharvest.component.ts
+++ src/app/pages/cdk/cdk-object-reharvest/cdk-object-reharvest.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+// --- data mock to delete ---
+export interface objectReharvest {
+  date: string;
+  pid: string;
+  deleteObject: boolean;
+  description: string;
+  state: string;
+}
+
+const ELEMENT_DATA: objectReharvest[] = [
+  {date: '20.4.2024', pid: 'uuid:3a99c250-bc5d-4318-a5a8-db00547fd5c4', deleteObject: true, description: 'Manualni reharvest objektu z MZK, KNAV', state: 'PLANNED'},
+  {date: '24.4.2024', pid: 'uuid:c9b6c867-6d60-4a64-9519-95f5e66ea910', deleteObject: false, description: 'Neplanovany reharvest objektu z duvodu dila/stranky', state: 'FINISHED'},
+  {date: '20.4.2024', pid: 'uuid:3a99c250-bc5d-4318-a5a8-db00547fd5c4', deleteObject: true, description: 'Manualni reharvest objektu z MZK, KNAV', state: 'PLANNED'},
+  {date: '24.4.2024', pid: 'uuid:c9b6c867-6d60-4a64-9519-95f5e66ea910', deleteObject: false, description: 'Neplanovany reharvest objektu z duvodu dila/stranky', state: 'FINISHED'}
+];
+// -- data mock to delete ---
+
 @Component({
   selector: 'app-cdk-object-reharvest',
   templateUrl: './cdk-object-reharvest.component.html',
   styleUrls: ['./cdk-object-reharvest.component.scss']
 })
 export class CdkObjectReharvestComponent implements OnInit {
+
+  displayedColumns: string[] = ['date', 'pid', 'state', 'deleteObject', 'description'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
