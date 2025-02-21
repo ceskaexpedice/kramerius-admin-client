e8c276c3ba8d8eadce2b2833c7e25f35b768bf85
Petr Kudela <kudy.samba@gmail.com>
2025-02-21 10:29:52 +0100

    https://github.com/ceskaexpedice/kramerius-admin-client/issues/138 - indexing / model - dialogs code refactoring, fxLayout has been removed

--- src/app/dialogs/schedule-indexations-by-multiple-pids-dialog/schedule-indexations-by-multiple-pids-dialog.component.ts
+++ src/app/dialogs/schedule-indexations-by-multiple-pids-dialog/schedule-indexations-by-multiple-pids-dialog.component.ts
@@ -11,13 +11,12 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
-import { FlexLayoutModule } from 'ngx-flexible-layout';
 import { AdminApiService } from 'src/app/services/admin-api.service';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, TranslateModule, FlexLayoutModule, FormsModule, MatDialogModule,
+  imports: [CommonModule, TranslateModule, FormsModule, MatDialogModule,
     MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, 
   MatProgressBarModule, MatTooltipModule, MatCheckboxModule],
   selector: 'app-schedule-indexations-by-multiple-pids-dialog',
