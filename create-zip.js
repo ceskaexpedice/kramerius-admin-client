57e06dce5051b9bf47ebef032f283c0254d01152
pavel-stastny <pavel.stastny@gmail.com>
2025-04-23 14:41:43 +0200

    Preparing for release

--- create-zip.js
+++ create-zip.js
@@ -2,28 +2,62 @@
 console.log("!!!! ARCHIVING ....");
 const fs = require('fs');
 const archiver = require('archiver');
+const path = require('path');
 
-if (!fs.existsSync(__dirname + '/dist-zip')) {
-    fs.mkdirSync(__dirname + '/dist-zip');
+
+const zipDir = path.join(__dirname, 'dist-zip');
+const outputPath = path.join(zipDir, 'kramerius-admin.zip');
+const inputDir = path.join(__dirname, 'dist/kramerius-admin-client/browser');
+
+console.log("Checking if zipDirExists " +zipDir);
+if (!fs.existsSync(zipDir)) {
+    fs.mkdirSync(zipDir, { recursive: true }); 
 }
 
-const output = fs.createWriteStream(__dirname + '/dist-zip/kramerius-admin.zip');
+function listFilesRecursive(dir, indent = '') {
+    const files = fs.readdirSync(dir);
+    files.forEach(file => {
+        const fullPath = path.join(dir, file);
+        const stats = fs.statSync(fullPath);
+        if (stats.isDirectory()) {
+            console.log(`${indent}[DIR]  ${file}`);
+            listFilesRecursive(fullPath, indent + '  ');
+        } else {
+            console.log(`${indent}[FILE] ${file}`);
+        }
+    });
+}
+
+
+
+const output = fs.createWriteStream(outputPath);
 const archive = archiver('zip', {
     zlib: { level: 9 }
 });
 
 
 output.on('close', function() {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
+	console.log(`${archive.pointer()} total bytes`);
+    console.log('Archiving complete. File descriptor closed.');
 });
 
 archive.on('error', function(err) {
     throw err;
 });
 
+console.log(`📂 Packing ${inputDir}:`);
+listFilesRecursive(inputDir);
+
 archive.pipe(output);
 
-archive.directory(__dirname + '/dist/kramerius-admin', 'kramerius-admin');
+
+archive.directory(inputDir, 'kramerius-admin', entry => {
+	const relPath = entry.name;
+    if (relPath.includes('assets/shared')) {
+        console.log(`⏭️ Excluding: ${relPath}`);
+        return false;
+    }
+    return entry;
+});
 
 archive.finalize();
